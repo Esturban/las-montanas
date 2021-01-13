cadb0d48dbfaf726b12c006ef52d858a1f780178
Esturban <valtaye@gmail.com>
2021-01-13 13:46:24 -0500

    CSS and JS
    Improving the website performance by using JS to load the relative style sheets async

--- static/js/main.js
+++ static/js/main.js
@@ -130,6 +130,20 @@ window.onload = function() {
       'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
     document.body.appendChild(script);
   }
+  
+          // to defer the loading of stylesheets
+        // just add it right before the </body> tag
+        // and before any javaScript file inclusion (for performance)  
+        function loadStyleSheet(src){
+            if (document.createStyleSheet) document.createStyleSheet(src);
+            else {
+                var stylesheet = document.createElement('link');
+                stylesheet.href = src;
+                stylesheet.rel = 'stylesheet';
+                stylesheet.type = 'text/css';
+                document.getElementsByTagName('head')[0].appendChild(stylesheet);
+            }
+        }
 
 
 });
